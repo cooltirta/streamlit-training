30221481435b0c230e787e09eac5054af251d034
cooltirta <79089187+cooltirta@users.noreply.github.com>
2024-09-09 07:20:10 +0000

    nambah hypertension

---
+++ page/hypertension_risk.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.title('**:material/cardiology: Early Detection Hypertension Risk**')
+
+st.markdown('''<a href="https://ai-platform-dev.dto.kemkes.go.id/" >Open in new tab <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20,300,0,0" /><span class="material-symbols-outlined">open_in_new</span>''', unsafe_allow_html=True)
+st.markdown('''<iframe src="https://ai-platform-dev.dto.kemkes.go.id/" title="description" style="height:550px; width:100%; border:solid; border-width: thin;" ></iframe>''', unsafe_allow_html=True)
