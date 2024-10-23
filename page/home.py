3a261b290c7d8a1679f723f41929ea633bb92bc3
cooltirta <79089187+cooltirta@users.noreply.github.com>
2024-10-23 04:19:44 +0000

    retry

--- page/home.py
+++ page/home.py
@@ -7,10 +7,12 @@ col1, col2, col3 = main_div.columns(3)
 col2.image('dash_logo.png')
 main_div.markdown(
     '''
-    <div style="text-align: justify; font-size:19px;">
-        &emsp;&emsp;Welcome to <b>DASH</b>, the project hub of the Data Science Team at DTO, Ministry of Health. Here, we showcase our work in applying data science to address key challenges in public health. From AI and machine learning to data visualization and predictive modeling, our projects are designed to provide real-world solutions with measurable impact.
-        <br>
-        &emsp;&emsp;Explore our projects through the menu on the left, and see our projects visualized. We’re excited to share our progress with you.
+    <div style="text-align: justify;">
+        Welcome to the DASH!! of the Data Science Team at DTO Ministry of Health <br><br> 
+        Here, we highlight our innovative work, where data meets creativity to solve real-world problems and creating beneficial impact to the society. Our projects span various domains, showcasing our expertise in AI, machine learning, data visualization, predictive modeling, and more. <br><br>
+        Whether you're here to explore our latest breakthroughs, gain insights into complex data challenges, or find inspiration for your own projects, you'll discover how we turn data into actionable insight. <br><br>
+        Kindly navigate the <u>left menu</u> to discover our projects. <br><br> 
+        Join us on this journey as we push the boundaries of what's possible with data.
     </div>
     ''', 
     unsafe_allow_html=True
@@ -20,7 +22,7 @@ col21, col22, col23, col24, col25 = main_div.columns(5)
 col23.markdown(
     '''
         <div style="text-align: center; padding-top: 50px;">
-            <i>Developed by:</i>
+            <b><i>Developed by:</i></b>
         </div>
     ''',
     unsafe_allow_html=True
