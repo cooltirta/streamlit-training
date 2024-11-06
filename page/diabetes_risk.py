ca8b861076acd3c16b146a590b1bdf0aa31cc471
cooltirta <79089187+cooltirta@users.noreply.github.com>
2024-11-06 09:09:38 +0000

    add gds and new form

--- page/diabetes_risk.py
+++ page/diabetes_risk.py
@@ -14,15 +14,28 @@ diabetes_password = st.secrets["DIABETES_PASSWORD"]
 # diabetes_password = os.getenv("DIABETES_PASSWORD")
 
 # set default values
+NAME = None
+NIK = None
+PLACE = None
 AGE = 15
 SYSTOLIC = 60
 FAMILY_HISTORY = None
 EXERCISE = None
 DIET = None
+GDS = 40
 
 # set default session var
 if "diabetes_password_correct" not in st.session_state:
     st.session_state.diabetes_password_correct = False
+
+if "name" not in st.session_state:
+    st.session_state.name = NAME
+
+if "nik" not in st.session_state:
+    st.session_state.nik = NIK
+
+if "place" not in st.session_state:
+    st.session_state.place = PLACE
 
 if "age" not in st.session_state:
     st.session_state.age = AGE
@@ -38,6 +51,9 @@ if "exercise" not in st.session_state:
 
 if "diet" not in st.session_state:
     st.session_state.diet = DIET
+
+if "gds" not in st.session_state:
+    st.session_state.gds = GDS
 
 # get model
 clf = joblib.load('./model_logic/dm_risk/lightgbm_ASIK_pickle_clean_5f.pkl')
@@ -166,7 +182,7 @@ st.markdown('''
 ''', unsafe_allow_html=True)
 
 # define title
-st.title('**:material/glucose: Early Detection Diabetes Risk**')
+# st.title('**:material/glucose: Early Detection Diabetes Risk**')
 
 # define main div
 div_main = st.container()
@@ -174,19 +190,16 @@ div_main = st.container()
 # define columns
 col_top1, col_top2, = div_main.columns(2, vertical_alignment="center")
 
-col_top1.markdown('<p class="medium50">Mari ketahui risiko diabetesmu!</p>', unsafe_allow_html=True)
+col_top1.markdown('<p class="medium50">Uji Coba Prediksi Risiko Diabetes</p>', unsafe_allow_html=True)
 col_top1.markdown('''
-    <p class="regular18" style="color:#505F98;">Diabetes merupakan penyakit yang berbahaya bagi tubuhmu.<br>
-    Mengetahui risiko diabetes tipe 2 anda hanya memerlukan beberapa menit.</p>\n
-    <p class="regular18" style="color:#505F98;">Sebelum mengisi, siapkan alat pengukur tekanan darah juga ya.</p>\n
-    <div style="max-width:fit-content; margin-left:auto; margin-right:auto;"><button class="cekrisikobtn" id="cekrisikobtn">Cek Risiko</button></div>
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0&icon_names=warning" />
+    <p class="regular18" style="color:#505F98;">Formulir uji coba tes risiko diabetes bertujuan untuk memvalidasi dan menyempurnakan pemodelan kecerdasan artifisial untuk mendeteksi diabetes berdasarkan 5 risiko yaitu usia, tekanan darah, riwayat keluarga, olahraga, dan konsumsi makan</p>\n
+    <div style="max-width:fit-content; margin-left:auto; margin-right:auto;"><button class="cekrisikobtn" id="cekrisikobtn">Isi Formulir</button></div>
 ''', unsafe_allow_html=True)
-col_top1.markdown('''
-    <div style="padding-bottom:10px;">
-        <div style="margin-left:auto; margin-right:auto; max-width: fit-content; background:#FFFCE7; padding:10px, 20px, 10px, 20px; color:#926c05; text-align:center;">⚠️ Catatan: Hasil prediksi ini hanya sebagai pemeriksaan dini, bukan diagnosis medis</div>
-    </div>
-''', unsafe_allow_html=True)
+# col_top1.markdown('''
+#     <div style="padding-bottom:10px;">
+#         <div style="margin-left:auto; margin-right:auto; max-width: fit-content; background:#FFFCE7; padding:10px, 20px, 10px, 20px; color:#926c05; text-align:center;">⚠️ Catatan: Hasil prediksi ini hanya sebagai pemeriksaan dini, bukan diagnosis medis</div>
+#     </div>
+# ''', unsafe_allow_html=True)
 col_top2.image('Frame_4.png')
 
 # Password Check
@@ -204,9 +217,9 @@ if not st.session_state["diabetes_password_correct"]:
             st.stop()  # Stop execution here if the password is incorrect
 else:
 
-    div_main.markdown('<p class="medium50" style="font-weight:700; text-align:center;" id="risiko">Yuk, isi form ini buat cek risiko DM2 kamu!</p>', unsafe_allow_html=True)
+    div_main.markdown('<p class="medium50" style="font-weight:700; text-align:center;" id="risiko">Isi formulir ini dengan data diri pasien</p>', unsafe_allow_html=True)
     div_main.divider()
-    div_main.markdown('<p class="formtitle" style="text-align:center;">Identitas diri</p>', unsafe_allow_html=True)
+    div_main.markdown('<p class="formtitle" style="text-align:center;">Identitas Pasien</p>', unsafe_allow_html=True)
 
 # define columns    
     col_top21, col_top22, col_top23 = div_main.columns(3, vertical_alignment="center")
@@ -214,7 +227,7 @@ else:
     col_top21.markdown('<p class="formquestion">Nama Pasien</p>', unsafe_allow_html=True)
     placeholder_name = col_top21.empty()
 
-    col_top22.markdown('<p class="formquestion">NIK</p>', unsafe_allow_html=True)
+    col_top22.markdown('<p class="formquestion">NIK Pasien</p>', unsafe_allow_html=True)
     placeholder_nik = col_top22.empty()
 
     col_top23.markdown('<p class="formquestion">Tempat pemeriksaan</p>', unsafe_allow_html=True)
@@ -232,30 +245,34 @@ else:
     placeholder_age = col_mid1.empty()
     col_mid1.markdown('<p class="regular16" style="color:#505F98;">Semakin bertambah usia, risiko terkena DM2 juga cenderung meningkat.</p>', unsafe_allow_html=True)
 
-    col_mid2.markdown('<p class="formquestion">Berapakah tekanan darah sistolik pasien?</p>', unsafe_allow_html=True)
+    col_mid2.markdown('<p class="formquestion">Berapa tekanan darah sistolik pasien?</p>', unsafe_allow_html=True)
     placeholder_systolic = col_mid2.empty()
-    col_mid2.markdown('<p class="regular16" style="color:#505F98;">Tekanan darah sistolik merupakan angka atas pada pengukuran tekanan darah. Peningkatan tekanan darah sistolik dapat meningkatkan potensi risiko DM2.</p>', unsafe_allow_html=True)
+    col_mid2.markdown('<p class="regular16" style="color:#505F98;">Tekanan sistol merupakan angka atas pada pengukuran tekanan darah. Peningkatan tekanan sistol dapat meningkatkan potensi risiko DM2.</p>', unsafe_allow_html=True)
 
     col_mid21, col_mid22 = div_main.columns(2, gap="large")
 
     col_mid21.markdown('<p class="formquestion" style="padding-top:50px;">Apakah pasien memiliki orang tua atau saudara kandung yang menderita diabetes?</p>', unsafe_allow_html=True)
     placeholder_family_history = col_mid21.empty()
-    col_mid21.markdown('<p class="regular16" style="color:#505F98;">Riwayat keluarga dengan diabetes dapat meningkatkan risiko Anda terkena DM2. Mengetahui faktor ini bisa membantu Anda lebih waspada dan mengambil langkah pencegahan yang tepat sejak dini.</p>', unsafe_allow_html=True)
+    col_mid21.markdown('<p class="regular16" style="color:#505F98;">Riwayat keluarga dengan diabetes dapat meningkatkan risiko kamu terkena DM2. Mengetahui faktor ini bisa membantu kamu lebih waspada dan mengambil langkah pencegahan yang tepat sejak dini..</p>', unsafe_allow_html=True)
 
     col_mid22.markdown('<p class="formquestion" style="padding-top:50px;">Apakah pasien rutin berolahraga, setidaknya 150 menit dalam seminggu?</p>', unsafe_allow_html=True)
     placeholder_exercise = col_mid22.empty()
     col_mid22.markdown('<p class="regular16" style="color:#505F98;">Memiliki kebiasaan berolahraga secara teratur dapat berpengaruh terhadap penurunan risiko terkena DM2.</p>', unsafe_allow_html=True)
 
-    col_mid31, col_mid32, col_mid33 = div_main.columns(3)
+    col_mid31, col_mid32 = div_main.columns(2, gap="large")
 
-    col_mid32.markdown('<p class="formquestion" style="padding-top:50px;">Apakah pasien mengkomsumsi 5 porsi buah atau sayur tiap harinya?</p>', unsafe_allow_html=True)
-    placeholder_diet = col_mid32.empty()
-    col_mid32.markdown('<p class="regular16" style="color:#505F98;">Pola makan yang sehat, termasuk rutin mengonsumsi buah dan sayur, dapat membantu mengurangi risiko terkena diabetes.</p>', unsafe_allow_html=True)
+    col_mid31.markdown('<p class="formquestion" style="padding-top:50px;">Apakah pasien mengkomsumsi 5 porsi buah atau sayur tiap harinya?</p>', unsafe_allow_html=True)
+    placeholder_diet = col_mid31.empty()
+    col_mid31.markdown('<p class="regular16" style="color:#505F98;">Pola makan yang sehat, termasuk rutin mengonsumsi buah dan sayur, dapat membantu mengurangi risiko terkena diabetes.</p>', unsafe_allow_html=True)
+
+    col_mid32.markdown('<p class="formquestion" style="padding-top:50px;">Berapakah gula darah sewaktu pasien saat ini?</p>', unsafe_allow_html=True)
+    placeholder_gds = col_mid32.empty()
+    col_mid32.markdown('<p class="regular16" style="color:#505F98;">Gula darah sewaktu pasien akan menjadi validator dalam uji coba ini.</p>', unsafe_allow_html=True)
 
     div_main.markdown('<p style="padding-top:100px;">&nbsp;</p>', unsafe_allow_html=True)
 
 # start modal dialog and def calculation
-    @st.dialog("Hasil Skrining", width="large")
+    @st.dialog("Data Berhasil Dikirim", width="large")
     def calculate_risk(obj):
         df = pd.DataFrame(obj)
 
@@ -284,58 +301,77 @@ else:
 
         df['prediction'] = result
 
-        url = f'''https://docs.google.com/forms/d/e/1FAIpQLSfWP-9GW9NSUL2gvae4c0xDZ1dxTfeh5k7t8QIWXy6POBUqHw/formResponse?usp=pp_url&entry.1911041054={obj['olahraga'][0]}&entry.104049788={obj['konsumsi_sayur_buah'][0]}&entry.185650305={obj['umur'][0]}&entry.1560480851={obj['sistol'][0]}&entry.761892027={obj['riwayat_keluarga_dm'][0]}&entry.1623306659={result}'''
+        url = f'''https://docs.google.com/forms/d/e/1FAIpQLSfWP-9GW9NSUL2gvae4c0xDZ1dxTfeh5k7t8QIWXy6POBUqHw/formResponse?usp=pp_url&entry.1461253234={obj['name'][0]}&entry.97617893={obj['nik'][0]}&entry.771091418={obj['place'][0]}&entry.1911041054={obj['olahraga'][0]}&entry.104049788={obj['konsumsi_sayur_buah'][0]}&entry.185650305={obj['umur'][0]}&entry.1560480851={obj['sistol'][0]}&entry.761892027={obj['riwayat_keluarga_dm'][0]}&entry.1623306659={result}&entry.955067944={probabilities[0]}&entry.1459946696={obj['gds'][0]}'''
         response = requests.get(url)
 
         res = '<b style="color:#CF2B2E;">Tinggi</b>' if result == 'Tinggi' else '<b style="color:#57855D;">Rendah</b>'''
         div_modal = st.container()
-        div_modal.markdown(f'''
-            <p class="medium50" style="text-align:center;">Kamu Beresiko {res}</p>\n
-            <p class="regular18" style="color:#505F98; background-color:#D2E0B7; text-align:center;">Hasil skrining bukan hasil pemeriksaan medis. Konsultasikan dengan tenaga profesional untuk informasi lanjutan</p>\n
-            <p class="regular18" style="text-align:center; font-weight:500; padding-top:50px;"><b>Mengenal risiko diabetes pada skrining ini:</b></p>
-        ''', unsafe_allow_html=True)
+        div_modal.success('Terima kasih kepada tenaga kesehatan dan relawan yang telah mengisi formulir pilot diabetes untuk validasi kecerdasan buatan dalam prediksi risiko DM', icon="✅")
 
-        col_mod1, col_mod2 = div_modal.columns(2)
-        col_mod1.markdown('''
-            <p class="medium24">Risiko yang tidak dapat dirubah</p>
-            <p class="regular16" style="color:#505F98;">Risiko ini tidak dapat dihindari, sehingga lebih fokuslah kepada faktor yang bisa berubah</p>
-            <ul>
-                <li class="inf">Umur</li>
-                <li class="inf">Riwayat keluarga</li>
-            </ul>
-        ''', unsafe_allow_html=True)
-
-        col_mod2.markdown('''
-            <p class="medium24">Risiko yang dapat dirubah</p>
-            <p class="regular16" style="color:#505F98;">Risiko ini dapat dikurangi atau dirubah dengan cara menjaga lifestyle saudara</p>
-            <ul>
-                <li class="inf">Tekanan darah sistolik</li>
-                <li class="inf">Olahraga</li>
-                <li class="inf">Konsumsi sayur-buah</li>
-            </ul>
-        ''', unsafe_allow_html=True)
-
-# define columns
-    col_mid41, col_mid42, col_mid43, col_mid44 = div_main.columns(4, vertical_alignment="center")
-
-    if col_mid42.button("**Cek Hasil**", type="primary"):
-        calculate_risk(
-            {
-                "umur": [st.session_state.age],
-                "sistol": [st.session_state.systolic],
-                "riwayat_keluarga_dm": [st.session_state.family_history],
-                "olahraga": [st.session_state.exercise],
-                "konsumsi_sayur_buah": [st.session_state.diet]
-            }
-
-        )
-
-    if col_mid43.button('**Skrining Ulang**', type='secondary'):
+        st.session_state.name = NAME
+        st.session_state.nik = NIK
+        st.session_state.place = PLACE
         st.session_state.age = AGE
         st.session_state.systolic = SYSTOLIC
         st.session_state.family_history = FAMILY_HISTORY
         st.session_state.exercise = EXERCISE
         st.session_state.diet = DIET
+        st.session_state.gds = GDS
+        # div_modal.markdown(f'''
+        #     <p class="medium50" style="text-align:center;">Kamu Beresiko {res}</p>\n
+        #     <p class="regular18" style="color:#505F98; background-color:#D2E0B7; text-align:center;">Hasil skrining bukan hasil pemeriksaan medis. Konsultasikan dengan tenaga profesional untuk informasi lanjutan</p>\n
+        #     <p class="regular18" style="text-align:center; font-weight:500; padding-top:50px;"><b>Mengenal risiko diabetes pada skrining ini:</b></p>
+        # ''', unsafe_allow_html=True)
+
+        # col_mod1, col_mod2 = div_modal.columns(2)
+        # col_mod1.markdown('''
+        #     <p class="medium24">Risiko yang tidak dapat dirubah</p>
+        #     <p class="regular16" style="color:#505F98;">Risiko ini tidak dapat dihindari, sehingga lebih fokuslah kepada faktor yang bisa berubah</p>
+        #     <ul>
+        #         <li class="inf">Umur</li>
+        #         <li class="inf">Riwayat keluarga</li>
+        #     </ul>
+        # ''', unsafe_allow_html=True)
+
+        # col_mod2.markdown('''
+        #     <p class="medium24">Risiko yang dapat dirubah</p>
+        #     <p class="regular16" style="color:#505F98;">Risiko ini dapat dikurangi atau dirubah dengan cara menjaga lifestyle saudara</p>
+        #     <ul>
+        #         <li class="inf">Tekanan darah sistolik</li>
+        #         <li class="inf">Olahraga</li>
+        #         <li class="inf">Konsumsi sayur-buah</li>
+        #     </ul>
+        # ''', unsafe_allow_html=True)
+
+# define columns
+    col_mid41, col_mid42, col_mid43, col_mid44 = div_main.columns(4, vertical_alignment="center")
+
+    if col_mid42.button("**Submit Hasil**", type="primary"):
+        calculate_risk(
+            {
+                "name": [st.session_state.name],
+                "nik": [st.session_state.nik],
+                "place": [st.session_state.place],
+                "umur": [st.session_state.age],
+                "sistol": [st.session_state.systolic],
+                "riwayat_keluarga_dm": [st.session_state.family_history],
+                "olahraga": [st.session_state.exercise],
+                "konsumsi_sayur_buah": [st.session_state.diet],
+                "gds": [st.session_state.gds]
+            }
+
+        )
+
+    if col_mid43.button('**Reset Form**', type='secondary'):
+        st.session_state.name = NAME
+        st.session_state.nik = NIK
+        st.session_state.place = PLACE
+        st.session_state.age = AGE
+        st.session_state.systolic = SYSTOLIC
+        st.session_state.family_history = FAMILY_HISTORY
+        st.session_state.exercise = EXERCISE
+        st.session_state.diet = DIET
+        st.session_state.gds = GDS
 
 # define input forms
     name = placeholder_name.text_input(
@@ -397,6 +433,14 @@ else:
         index=None,
         label_visibility="collapsed",
         key="diet"
+    )
+
+    gds = placeholder_gds.number_input(
+        "Berapakah gula darah sewaktu pasien saat ini?",
+        min_value=40,
+        max_value=650,
+        label_visibility="collapsed",
+        key="gds"
     )
 
 # Javascripts
