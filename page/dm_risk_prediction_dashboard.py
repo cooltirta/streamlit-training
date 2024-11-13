a369d12653f7b7285a5f8e3a652207b0d1a69d78
cooltirta <79089187+cooltirta@users.noreply.github.com>
2024-11-13 04:36:19 +0000

    nambahin dashboard dm dan stunting

---
+++ page/dm_risk_prediction_dashboard.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.title('**:material/space_dashboard: Diabetes Risk Prediction Dashboard**')
+
+st.markdown('''<a href="https://lookerstudio.google.com/reporting/0974db57-7691-4674-8914-14ead70c07d9" >Open in new tab <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20,300,0,0" /><span class="material-symbols-outlined">open_in_new</span>''', unsafe_allow_html=True)
+st.markdown('''<iframe src="https://lookerstudio.google.com/embed/reporting/0974db57-7691-4674-8914-14ead70c07d9/page/r0UIE" title="description" style="height:550px; width:100%; border:solid; border-width: thin;" ></iframe>''', unsafe_allow_html=True)
---
+++ page/stunting_prediction_dashboard.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.title('**:material/space_dashboard: Stunting Prediction Dashboard**')
+
+st.markdown('''<a href="https://lookerstudio.google.com/reporting/2514dd35-4b24-496c-abfe-bf72850aebb2" >Open in new tab <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20,300,0,0" /><span class="material-symbols-outlined">open_in_new</span>''', unsafe_allow_html=True)
+st.markdown('''<iframe src="https://lookerstudio.google.com/embed/reporting/2514dd35-4b24-496c-abfe-bf72850aebb2/page/p_nsprqn53jd" title="description" style="height:550px; width:100%; border:solid; border-width: thin;" ></iframe>''', unsafe_allow_html=True)
